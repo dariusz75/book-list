aeba04945a13422c9b085a7b39da761314593348
Dariusz Franczak <darius32@op.pl>
2018-05-22 00:32:04 +0100

    work in progress book added

--- app.js
+++ app.js
@@ -15,24 +15,39 @@ function UI() {}
 
 //Add book to list
 UI.prototype.addBookToList = function(book) {
+    var list = document.getElementById('book-list');
+    //Create tr element
+    var row = document.createElement('tr');
 
+    //Insert column
+    row.innerHTML = '<td>' + book.title + '</td><td>' + book.author + '</td><td>' + book.isbn + '</td><td> <a href="#" class="delete">x</a></td>' 
+    list.appendChild(row);
 }
 
+//Clear form fields
+UI.prototype.clearFields = function() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
 
 //Event listeners
 bookForm.addEventListener('submit', function(e){
     e.preventDefault();
-    var title = document.getElementById('title').value;
-    var author = document.getElementById('author').value;
-    var isbn = document.getElementById('isbn').value;
+    //Get form values
+    var titleToAdd = document.getElementById('title').value;
+    var authorToAdd = document.getElementById('author').value;
+    var isbnToAdd = document.getElementById('isbn').value;
 
     //Instance of new book
-    var book = new Book(title, author, isbn);
+    var book = new Book(titleToAdd, authorToAdd, isbnToAdd);
     
     //Instance of new UI
     var ui = new UI();
 
     //Add book to list
+    ui.addBookToList(book);
 
-
+    //Clear form fields
+    ui.clearFields();
 })
